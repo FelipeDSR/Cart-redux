28900d3cecb2e5dff0c7e25c2bd61e3b1e3b7389
Felipe Rodrigues <leepe.fr@gmail.com>
2021-02-25 21:35:23 -0300

    Updates

---
+++ precache-manifest.d5a891139eae839e1bc6687b41a4867b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3efeec3eb0c723d8cc1abca92342bb3f",
+    "url": "/Cart-redux/index.html"
+  },
+  {
+    "revision": "6bd9310622c49a82ce1a",
+    "url": "/Cart-redux/static/js/2.5cb21f9e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Cart-redux/static/js/2.5cb21f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dc7ae6252a63ee44aa9",
+    "url": "/Cart-redux/static/js/main.096aa8b1.chunk.js"
+  },
+  {
+    "revision": "d4388cc695325d7e7ee0",
+    "url": "/Cart-redux/static/js/runtime-main.1f2111c4.js"
+  }
+]);
